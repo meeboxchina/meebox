292eca243a3928ad62d6edda0995edeac724acd1
Sunny.Sun <sunyu.cn@gmail.com>
2014-08-27 23:57:13 +0800

    login

--- src/cn/meebox/Loading.java
+++ src/cn/meebox/Loading.java
@@ -2,11 +2,17 @@ package cn.meebox;
 
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClients;
@@ -38,6 +44,9 @@ public class Loading extends Activity {
 	private EditText login_username;
 	private EditText login_password;
 	private Button btnLogin;
+	private Button btnRegist;
+	private Button btnForgot;
+	
 
 	MyHandler myHandler;
 	
@@ -75,13 +84,21 @@ public class Loading extends Activity {
 		
 		myHandler = new MyHandler();
 		
-		btnLogin = (Button)findViewById(R.id.btnLogin);//获取按钮资源    
+		btnLogin = (Button)findViewById(R.id.btnLogin);//登录按钮  
 		btnLogin.setOnClickListener(new Button.OnClickListener(){//创建监听    
             public void onClick(View v) {    
             	MyThread m = new MyThread();
             	new Thread(m).start();
             }    
-        });  
+        }); 
+		
+		btnRegist = (Button)findViewById(R.id.btnRegist);//注册按钮  
+		btnRegist.setOnClickListener(new Button.OnClickListener(){//创建监听    
+            public void onClick(View v) {    
+            	Intent i = new Intent(Loading.this, RegistActivity.class);
+            	startActivity(i);
+            }    
+        }); 
 	}
 	
 	class MyHandler extends Handler {
@@ -101,8 +118,10 @@ public class Loading extends Activity {
             Bundle b = msg.getData();
             
             int httpcode = b.getInt("httpcode");
+            String json = b.getString("json");
             
             if(httpcode == 200){
+            	Toast.makeText(getApplicationContext(), json, Toast.LENGTH_SHORT).show();
             	Intent i = new Intent(Loading.this, MainActivity.class);
             	startActivity(i);
             }
@@ -114,23 +133,63 @@ public class Loading extends Activity {
 
 	class MyThread implements Runnable {
 		public void run() {
+			String password = null;
+			try {
+				MessageDigest digest = java.security.MessageDigest.getInstance("SHA-1");
+				digest.update(password.getBytes());
+				byte messageDigest[] = digest.digest();
+				StringBuffer hexString = new StringBuffer();
+				// 字节数组转换为 十六进制 数
+				for (int i = 0; i < messageDigest.length; i++) {
+					String shaHex = Integer.toHexString(messageDigest[i] & 0xFF);
+					if (shaHex.length() < 2) {
+						hexString.append(0);
+					}
+					hexString.append(shaHex);
+				}
+			} catch (NoSuchAlgorithmException e) {
+				e.printStackTrace();
+			}
 			
 			Log.d("thread:", "myThread");
 			Message msg = new Message();
 			Bundle b = new Bundle();// 存放数据
 			
-			String uri = "http://www.baidu.com/";
 			
-			HttpClient httpClient = new DefaultHttpClient(); 
 			
+			
+			String uri = "http://meebox.cn/login";
+			
+			//HttpClient httpClient = new DefaultHttpClient(); 
+			
+			DefaultHttpClient httpClient = new DefaultHttpClient();   
 			//CloseableHttpClient httpclient = HttpClients.createDefault();
 			
 	        HttpGet httpget = new HttpGet(uri);
 	        
-	        HttpResponse response;
-			try {
-				response = httpClient.execute(httpget);
-				b.putInt("httpcode", response.getStatusLine().getStatusCode());
+	        HttpPost httppost = new HttpPost(uri);   
+	        try {
+				StringEntity reqEntity = new StringEntity("username=sunyu&password=sunyu");
+				reqEntity.setContentType("application/x-www-form-urlencoded");   
+		        // 设置请求的数据   
+		        httppost.setEntity(reqEntity);   
+		        // 执行   
+		        HttpResponse response = httpClient.execute(httppost);  
+		        
+		        int httpcode = response.getStatusLine().getStatusCode();
+		        
+		        String json = null;
+		        
+		        HttpEntity entity = response.getEntity(); 
+		        
+		        json = entity.toString();
+		        
+		        b.putInt("httpcode", httpcode);
+		        b.putString("json", json);
+		        
+			} catch (UnsupportedEncodingException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
 			} catch (ClientProtocolException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -139,6 +198,7 @@ public class Loading extends Activity {
 				e.printStackTrace();
 			}
 	        
+	     
 			msg.setData(b);
 			Loading.this.myHandler.sendMessage(msg); // 向Handler发送消息,更新UI
 		}
---
+++ src/cn/meebox/android/package-info.java
@@ -0,0 +1,16 @@
+/**   
+ * @Title: package-info.java 
+ * @Package cn.meebox.android 
+ * @Description: TODO
+ * @author Sunny Sun sunyu@gmail.com 
+ * @date 2014?8?27? ??10:12:10 
+ * @version V1.0   
+ */
+/** 
+ * @ClassName: package-info 
+ * @Description: TODO
+ * @author Sunny Sun sunyu.cn@gmail.com
+ * @date 2014?8?27? ??10:12:10 
+ *  
+ */
+package cn.meebox.android;
